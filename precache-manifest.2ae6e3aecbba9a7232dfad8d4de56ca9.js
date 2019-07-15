d6d607e4314d45de2e47561a1d82877e7dc927ed
Giel <g.cobben@gmail.com>
2019-07-15 09:14:39 +0200

    Updates

---
+++ precache-manifest.2ae6e3aecbba9a7232dfad8d4de56ca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b759c6156737c38c294e",
+    "url": "/use-typewriter/static/js/runtime~main.d59cfdb5.js"
+  },
+  {
+    "revision": "14070adfc22ff5d52cb5",
+    "url": "/use-typewriter/static/js/main.31b0669a.chunk.js"
+  },
+  {
+    "revision": "148fc5ef9907566254aa",
+    "url": "/use-typewriter/static/js/2.52ed8df3.chunk.js"
+  },
+  {
+    "revision": "14070adfc22ff5d52cb5",
+    "url": "/use-typewriter/static/css/main.698fa813.chunk.css"
+  },
+  {
+    "revision": "2976daee010fc5dea76b4a4c6c7498fb",
+    "url": "/use-typewriter/index.html"
+  }
+];
