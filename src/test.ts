fe0d7f0e07a3a0474729f7958472ce5219842505
Giel Cobben <g.cobben@gmail.com>
2019-07-18 08:16:21 +0200

    Removed test

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import useTypewriter, { typewriterProps } from "./";
-
-describe("useTypewriter", () => {
-  it("Should return currentWord", () => {
-    const currentWord = useTypewriter({ words: ["Giel"] });
-    console.log("TEST", currentWord);
-  });
-});
